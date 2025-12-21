f428d3ee6a478334d095ed7b13b155b8baaa2386
Kazi Waziullah <waziullah@live.com>
2025-12-21 18:16:04 +0600

    update: OAuth Permission scope function

---
+++ UserServiceGrpc/Models/Entities/RoleAccess.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Metadata.Ecma335;
+
+namespace UserServiceGrpc.Models.Entities
+{
+    [Table("RoleAccess")]
+    public class RoleAccess
+    {
+        public RoleAccess()
+        {
+            
+        }
+        public int RoleId { get; set; }
+
+        [Column(TypeName ="varchar(40)")]
+        public string RoleDetail { get; set; }
+
+        [Column(TypeName = "varchar(40)")]
+        public string ModuleName { get; set; }
+
+        [Column(TypeName = "varchar(40)")]
+        public string FrmDetail { get; set; }
+
+        public bool ViewPermission { get; set; }
+        public bool AddPermission { get; set; }
+        public bool EditPermission { get; set; }
+        public bool DeletePermission { get; set; }
+    }
+}
